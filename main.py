3492a8ecd90947b70aa69ca250214750204b3de8
Gustavo Hernández <138713363+Ship-Codex@users.noreply.github.com>
2025-02-16 17:34:52 -0600

    Primer disenio de la interfaz

--- main.py
+++ main.py
@@ -13,6 +13,54 @@ class MainWindow(QMainWindow):
 
         self.show()
 
+    def dataGrafic(self):
+        plt
+
+    
+    # def paintEvent(self, event):
+    #     """ Dibuja los ejes y los puntos en el plano cartesiano """
+    #     painter = QPainter(self)
+    #     painter.setRenderHint(QPainter.Antialiasing)
+
+    #     # Dibujar ejes
+    #     width = self.width()
+    #     height = self.height()
+    #     center_x = width // 2
+    #     center_y = height // 2
+
+    #     pen = QPen(Qt.black, 2)
+    #     painter.setPen(pen)
+    #     painter.drawLine(0, center_y, width, center_y)  # Eje X
+    #     painter.drawLine(center_x, 0, center_x, height)  # Eje Y
+
+    
+    # def mousePressEvent(self, event: QMouseEvent):
+    #     """ Captura la posición del clic y la almacena """
+    #     if event.button() == Qt.LeftButton:
+
+    #         # Objeto de tipo QPoint
+    #         print(f"({event.position().toPoint()})")
+    #         self.update()
+
+    # def mousePressEvent(self, event):
+    #     if event.button() == Qt.LeftButton:
+    #         widget_width = self.width()
+    #         widget_height = self.height()
+
+    #         # Centro del widget es el (0,0) en el sistema cartesiano
+    #         center_x = widget_width // 2
+    #         center_y = widget_height // 2
+
+    #         # Obtener coordenadas del clic
+    #         click_x = event.position().toPoint().x()
+    #         click_y = event.position().toPoint().y()
+
+    #         # Convertir a coordenadas cartesianas
+    #         cart_x = click_x - center_x
+    #         cart_y = -(click_y - center_y)  # Invertimos Y porque en Qt Y crece hacia abajo
+
+    #         print(f"Coordenada en el sistema cartesiano: ({cart_x}, {cart_y})")
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
--- module/main_ui.py
+++ module/main_ui.py
@@ -15,25 +15,128 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QMainWindow, QMenuBar, QSizePolicy, QSlider,
+    QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(642, 494)
+        MainWindow.resize(735, 483)
         MainWindow.setStyleSheet(u"")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(2, 2, -1, -1)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(500, 200))
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.layaout_grafic = QVBoxLayout()
+        self.layaout_grafic.setObjectName(u"layaout_grafic")
+
+        self.horizontalLayout.addLayout(self.layaout_grafic)
+
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setStyleSheet(u"background-color: rgb(0, 170, 127);")
+        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.label = QLabel(self.frame_2)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer_3)
+
+        self.label_2 = QLabel(self.frame_2)
+        self.label_2.setObjectName(u"label_2")
+
+        self.verticalLayout_2.addWidget(self.label_2)
+
+        self.slider_weight_01 = QSlider(self.frame_2)
+        self.slider_weight_01.setObjectName(u"slider_weight_01")
+        self.slider_weight_01.setOrientation(Qt.Orientation.Horizontal)
+
+        self.verticalLayout_2.addWidget(self.slider_weight_01)
+
+        self.label_5 = QLabel(self.frame_2)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label_5)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer_2)
+
+        self.label_3 = QLabel(self.frame_2)
+        self.label_3.setObjectName(u"label_3")
+
+        self.verticalLayout_2.addWidget(self.label_3)
+
+        self.slider_weight_02 = QSlider(self.frame_2)
+        self.slider_weight_02.setObjectName(u"slider_weight_02")
+        self.slider_weight_02.setOrientation(Qt.Orientation.Horizontal)
+
+        self.verticalLayout_2.addWidget(self.slider_weight_02)
+
+        self.label_6 = QLabel(self.frame_2)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label_6)
+
+        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer_4)
+
+        self.label_4 = QLabel(self.frame_2)
+        self.label_4.setObjectName(u"label_4")
+
+        self.verticalLayout_2.addWidget(self.label_4)
+
+        self.slider_bias = QSlider(self.frame_2)
+        self.slider_bias.setObjectName(u"slider_bias")
+        self.slider_bias.setOrientation(Qt.Orientation.Horizontal)
+
+        self.verticalLayout_2.addWidget(self.slider_bias)
+
+        self.label_7 = QLabel(self.frame_2)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label_7)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+
+        self.horizontalLayout.addWidget(self.frame_2)
+
+        self.horizontalLayout.setStretch(0, 3)
+        self.horizontalLayout.setStretch(1, 2)
+
+        self.verticalLayout.addWidget(self.frame)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 642, 33))
+        self.menubar.setGeometry(QRect(0, 0, 735, 33))
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
 
@@ -42,5 +145,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Perceptron", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Peso 01", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"00", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Peso 02", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"11", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Bias", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"22", None))
     # retranslateUi
 
